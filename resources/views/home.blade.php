0876042be958478bbd241d4a32e8584bb5522dae
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-10-09 22:29:45 -0300

    CRUD - v1.3 - first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,8 @@
 @extends('master')
 
+@section('css')
+    {{ url('css/home.css') }}
+@endsection
 @section('content')
   <h2>Home</h2>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', [HomeController::class,'index'])-> name('home');
+
+
 Route::get('/users', [UserController::class,'index'])->name('users.index');
 Route::post('/users/create', [UserController::class,'create'])->name('users.create');
 Route::post('/users', [UserController::class,'store'])->name('users.store');
